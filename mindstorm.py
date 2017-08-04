0d7aab0ba1badb19b86c982bc2e28ddb7b3c69dc
NaelaTarannum <naelatarannum@gmail.com>
2017-08-04 17:14:34 +0530

    Update mindstorm.py

--- mindstorm.py
+++ mindstorm.py
@@ -5,14 +5,7 @@ def draw_square(some_turtle):
       some_turtle.right(120)
       some_turtle.forward(100)
       some_turtle.right(60)
-#def draw_circle(some_turtle):
-    #some_turtle.circle(100)
-#def draw_tri(tom):
-#    tom.forward(100) 
-#    tom.left(120)
-#    tom.forward(100)
-#    tom.left(120)
-#    tom.forward(100)
+
 def draw():
     naela=turtle.Turtle()
     window=turtle.Screen()
@@ -25,18 +18,7 @@ def draw():
       naela.right(10)
     naela.right(90)
     naela.forward(200)
-    #rhys=turtle.Turtle()
-    #naela.goto(50)
-    #rhys.penup()
-    #rhys.backward(50)
-    #rhys.pendown()
-    #rhys.shape("turtle")
-    #rhys.color("black")
-    #draw_circle(rhys)
-    #rhys.penup()
-    #rhys.backward(200)
-    #rhys.pendown()
-    #draw_tri(rhys)
+    
 
     window.exitonclick()
 draw()
